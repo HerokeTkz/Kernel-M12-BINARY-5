00dc1ad39dafd670709e14b7737b832f40a247ea
HEROKE TKZ <bladetrinity466@gmail.com>
2024-12-21 18:40:45 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 
+export PATH=/aosp-toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/:/aosp-toolchain/clang/host/linux-x86/clang-r437112/bin/:$PATH
+
 export PLATFORM_VERSION=13
-export ANDROID_MAJOR_VERSION=t
+export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
 
-make ARCH=arm64 exynos850-m12nsxx_defconfig
-make ARCH=arm64 -j64
+
+make ARCH=arm64 HerokeTKZ_defconfig
+make
