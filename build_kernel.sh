ab1c94bc9068a9e792e43cbcb1c1a4fba1a96fe8
HEROKE TKZ <bladetrinity466@gmail.com>
2024-12-21 23:46:12 -0300

    FIX build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-export PATH=/aosp-toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/:/aosp-toolchain/clang/host/linux-x86/clang-r437112/bin/:$PATH
+export PATH=/aosp-toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/:/aosp-toolchain/clang/host/linux-x86/clang-r416183c/bin/:$PATH
 
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
 
 
-make ARCH=arm64 HerokeTKZ_defconfig
-make
+mkdir out
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-androidkernel- CLANG_TRIPLE=aarch64-linux-gnu- HerokeTKZ_defconfig
+make ARCH=arm64 O=out CROSS_COMPILE=aarch64-linux-androidkernel- CLANG_TRIPLE=aarch64-linux-gnu- -j64
